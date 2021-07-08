b4f871cff9fa1e768065c27ac9fe824816c92afb
marcelo ivan <sagacello@hotmail.com>
2021-07-08 17:02:32 -0300

    preenchendo o banco com as seeders

---
+++ src/config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'devices_management',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
