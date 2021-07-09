e3ebe7f006085b1e92d25747462dc06e0349ec6f
marcelo ivan <sagacello@hotmail.com>
2021-07-09 14:08:37 -0300

    update e delete device funcionando corretamente

--- src/routes/devicesRoute.js
+++ src/routes/devicesRoute.js
@@ -6,5 +6,7 @@ const devicesControllers = require('../controllers/devicesControllers');
 router.post('/devices', devicesControllers.createDevice);
 router.get('/devices', devicesControllers.getAllDevices);
 router.get('/devices/:id', devicesControllers.getDeviceById);
+router.delete('/devices/:id', devicesControllers.deleteDeviceById);
+router.patch('/devices/:id', devicesControllers.updateDevice);
 
 module.exports = router;
